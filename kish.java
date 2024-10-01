10c3bf31faddea7be7e679151c54480dadc5ca8a
mrKinzo <eduardokinzo@gmail.com>
2024-10-01 11:57:35 -0300

    editado por k15H , soma de dois vAl0R35

--- kish.java
+++ kish.java
@@ -1,10 +1,16 @@
 public class kish {
 
     public static void main(String[] args) {
-        while (true) {
+    //     while (true) {
             
         
-        System.out.println("[as]");
-    }
+    //     System.out.println("[as]");
+    // }
+int a =0;
+int c=1;
+while (true) {
+    int j= a+c;
+    System.out.println(j);
+}
 }
 }

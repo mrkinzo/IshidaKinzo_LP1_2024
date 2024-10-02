6d9dc556cd6345029381884420d4e2c9f372497a
mrKinzo <eduardokinzo@gmail.com>
2024-10-02 08:04:49 -0300

    abuu

---
+++ new_File_Kish.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class new_File_Kish {
+ 
+  public static void main(String[] args) {
+    
+    int x=5;
+    int y=5;
+
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            if (i>=j) {
+              System.out.print('X');  
+            }else{
+                System.out.print('O');
+            }
+        }
+        System.out.println();
+    }
+  } 
+}

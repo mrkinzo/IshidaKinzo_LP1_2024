92e39786da8ae038b5e94177dddfb8f139ba9577
mrKinzo <eduardokinzo@gmail.com>
2024-10-15 20:42:14 -0300

    tentar denovo

--- BC_2311.java
+++ BC_2311.java
@@ -5,43 +5,48 @@ public class BC_2311 {
     public static Scanner Scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int competidores = 0;
-        competidores = Scan.nextInt();
-
-        for (int i = 0; i < competidores; i++) {
-            calcularSaltos();
-        }
-    }
-
-    public static void calcularSaltos() {
         float grauDif = 0;
         float notaFinal = 0;
         float nota = 0;
         float somaNota = 0;
-    float maiorNota = 0;
-    float menorNota = 0;
+        float maiorNota = 0;
+        float menorNota = 10;
         String nome = null;
+        int competidores = 0;
+        
+        competidores = Scan.nextInt();
+
+        for (int i = 0; i < competidores; i++) {
+            calcularSaltos(grauDif, notaFinal, nota, somaNota, maiorNota, menorNota, nome);
+        }
+    }
+
+    public static void calcularSaltos(float grauDif, float notaFinal, float nota, float somaNota, float maiorNota,
+            float menorNota, String nome) {
+
+        nome = Scan.next();
 
         grauDif = Scan.nextFloat();
 
-        for (int i = 0; i < 7; i++) {
-            nome = Scan.nextLine();
+        for (int j = 0; j < 7; j++) {
+
             nota = Scan.nextFloat();
             if (nota > maiorNota) {
-                maiorNota += nota;
+                maiorNota = nota;
             }
             if (nota < menorNota) {
-                menorNota += nota;
-            } else {
-                somaNota += nota;
+                menorNota = nota;
+            }else {
+                somaNota+=nota;
             }
 
             notaFinal = somaNota * grauDif;
 
-            printCompetitdor(nome, notaFinal);
+          printDados(nome, notaFinal);
         }
+    
     }
-    public static void printCompetitdor(String nome ,float notaFinal){
-        System.out.printf("%s %.2f\n", nome, notaFinal);
+    public static void printDados(String x , float a){
+        System.out.printf("%s %.2f\n", x, a);
     }
 }
